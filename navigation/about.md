6a844335d881a12ef86b1bc826728ea6a074a9a4
SlothInTheHat <lalwaninikhil42@gmail.com>
2024-08-29 10:45:52 -0700

    test

--- navigation/about.md
+++ navigation/about.md
@@ -6,3 +6,4 @@ permalink: /about/
 
 # About this page
 This Website will have all the things I make and do in CSA this trimester and possibly even later. 
+
--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,32 +1,42 @@
+#create variables for project locaiton and github repo
 export project_dir=/home/nikhil/nighthawk  # change nighthawk to different name to test your git clone
 export project=$project_dir/Nikhil_2025  # change portfolio_2025 to name of project from git clone
 export project_repo="https://github.com/SlothInTheHat/Nikhil_2025.git"  # change to project you created from portfolio_2025 template 
 
+
+#print out these variables in the console
 echo "Project dir: $project_dir"
 echo "Project: $project"
 echo "Repo: $project_repo"
 echo ""
 
+
+#print ruby version
 echo "RUBY VERSION:"
 ruby -v
 echo ""
 
+#print bundle version
 echo "BUNDLE VERSION:"
 bundle -v
 echo ""
 
+#print pyhton version
 echo "PYTHON VERSION:"
 python --version
 echo ""
 
+#print ruby packages
 echo "JUPYTER PACKEGES:"
 jupyter --version
 echo ""
 
 echo "NOW SETTING UP VIRTUAL ENVIORMENT"
 
+#run virtual enciorment script
 ./venv.sh
 
+#activate virtual enviorment script
 source venv/bin/activate
 echo ""
 
